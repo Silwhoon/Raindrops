f6db4587b15038b5b9c5e9afe2f1a8ae3b23497d
Duplesis <59741317+Aratolle@users.noreply.github.com>
2022-05-17 11:55:59 +0100

    Project cleanup
    Added description & information to the convertNumber function and tidied up a few comments.
    
    Added a readme file.

---
+++ Raindrops/readme.md
@@ -0,0 +1,15 @@
+# Tech Test
+## Raindrops
+A project that takes an integer as its input and converts it to a string containing various sounds of a raindrop based on its factors.
+
+## Description
+This project will take any integer input and return a string based on its value. 
+- If the input has a factor of 3, the returned string will have 'Pling' appended to it.
+- If the input has a factor of 5, the returned string will have 'Plang' appended to it.
+- If the input has a factor of 7, the returned string will have 'Plong' appended to it.
+- If the input does not have a factor of 3, 5 or 7 the number will be returned as a string.
+
+## Examples
+- 28's factors are 1, 2, 4, 7, 14 and 28: this would return a simple 'Plong'
+- 30's factors are 1, 2, 3, 5, 6, 10, 15, 30: this would return a 'PlingPlang'
+- 34 has four factors: 1, 2, 17, and 34: this would return '34'
--- Raindrops/src/main/java/spartaglobal/raindrop/Raindrop.java
+++ Raindrops/src/main/java/spartaglobal/raindrop/Raindrop.java
@@ -2,13 +2,19 @@ package spartaglobal.raindrop;
 
 public class Raindrop {
 
+    /**
+     * Takes an integer and converts it to a desired string based on if the integer has a factor of 3, 5 or 7
+     *
+     * @param input The integer to be converted.
+     * @return The desired string based on input.
+     */
     public static String convertNumber(int input) {
-        // Check if the input has a factor of 3, 5 or 7. If it doesn't, we can return out early
+        // Check if the input has a factor of 3, 5 or 7. If it doesn't we can return out early!
         if (input % 3 != 0 && input % 5 != 0 && input % 7 != 0) {
             return "" + input;
         }
 
-        // Otherwise, append pling, plang and/or plong based on the inputs factors of 3, 5 and 7
+        // Otherwise, append Pling, Plang and/or Plong based on the inputs' factors of 3, 5 and 7
         String ret = "";
         if (input % 3 == 0) ret += "Pling";
         if (input % 5 == 0) ret += "Plang";
